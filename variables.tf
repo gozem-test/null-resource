cca04b633ae1479ac273ec4b51e1723780e6e35c
Stéphane Noutsa <stephanenoutsak@gmail.com>
2023-11-24 15:56:38 +0100

    Adds aws credential variables

--- variables.tf
+++ variables.tf
@@ -1,3 +1,20 @@
+variable "AWS_ACCESS_KEY_ID" {
+  type = string
+}
+
+variable "AWS_SECRET_ACCESS_KEY" {
+  type = string
+}
+
+variable "AWS_SESSION_TOKEN" {
+  type    = string
+  default = null
+}
+
+variable "AWS_REGION" {
+  type = string
+}
+
 variable "commands" {
   type        = list(string)
   description = "List of commands that will trigger callback functions"
